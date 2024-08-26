ed675aecb83e3c8031d2b03906d75e20bf76ad99
manuelcodev <manuel.dev9604@gmail.com>
2024-08-26 14:41:29 -0500

    prueba ec2

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.get((req, res) => {
+    res.send('hello word')
+})
+
+app.listen(app.get('port'),()=>[
+    console.log('server on port 3000')
+])
